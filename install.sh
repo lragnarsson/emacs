d71f85a48313c1f224a8f3e67e8657908729185a
Lage Ragnarsson <lage.ragnarsson@gmail.com>
2016-07-17 16:54:55 +0200

    Include sensible-defaults in project. Improved install script.

--- install.sh
+++ install.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
-ln -s ~/Development/emacs/emacs.d ~/.emacs.d
-
-echo "Created symbolic link to ~/.emacs.d"
+if [ -d "$HOME/.emacs.d" ] ; then
+    echo "$HOME/.emacs.d already exist. Remove or move it first."
+else
+    ln -s "$(cd "$(dirname "$0")" && pwd)/emacs.d/" "$HOME/.emacs.d"
+    echo "Created symbolic link to ~/.emacs.d"
+fi
