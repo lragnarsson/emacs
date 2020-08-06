220f087a262c5b2efdc30219bd054d3c8441e834
Lage Ragnarsson <lage.ragnarsson@gmail.com>
2020-08-06 10:51:12 +0200

    Comment out unused stuff, add mac install instructions.

--- README.md
+++ README.md
@@ -3,6 +3,8 @@ Emacs config
 
 ## Installation
 1. Install emacs
+   - brew cask tap railwaycat/emacsmacport
+   - brew cask install emacs-mac
 2. Clone or download project.
 3. run install script. If you have started emacs before you need to remove the ~/.emacs.d folder and .emacs file first.
 4. open emacs and install the package `use-package`
--- emacs.d/init.el
+++ emacs.d/init.el
@@ -11,3 +11,17 @@
 (setq cider-cljs-lein-repl
       "(do (user/run)
            (user/browser-repl))")
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (virtualenvwrapper js2-mode rainbow-delimiters powerline paredit idle-highlight-mode cider yasnippet web-mode multiple-cursors gruvbox-theme zenburn-theme monokai-theme expand-region glsl-mode company use-package))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
